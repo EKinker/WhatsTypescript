67425abcd42d0a28b6c5affb4fc1868d9ba90ea9
Liz Kinker <Liz.Kinker@gmail.com>
2023-08-13 16:34:46 -0400

    Trying things

--- index.ts
+++ index.ts
@@ -1,15 +1,67 @@
 // Import stylesheets
-import './style.css';
+//import './style.css';
 
 
 const form: HTMLFormElement = document.querySelector('#defineform');
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
-
-form.onsubmit = () => {
+  
+form.onsubmit = async (event) => {
+  event?.preventDefault();
   const formData = new FormData(form);
 
   console.log(formData);
   const text = formData.get('defineword') as string;
   console.log(text);
-  return false; // prevent reload
+  //return false; // prevent reload
+
+  try {
+    const response = await fetch(url+text);  //check this
+    
+    if (!response.ok) {
+      throw new Error('Request failed'+ response.status);
+    }
+
+    const data: ApiResponse[] = await response.json();
+    console.log('Response: ', data)
+    // add more
+  
+  } catch (error) {
+    console.error('Oh no! ', error);
+  }
 };
+
+interface ApiResponse {
+  word: string;
+  phonetics: Phonetic[];
+  meanings: Definition[];
+  license: {
+    name: string;
+    url: string;
+  };
+  sourceUrls: string[];
+}
+
+interface Phonetic {
+  audio: string;
+  text: string;
+  sourceUrl?: string;
+  license?: {
+    name: string;
+    url: string;
+  };
+}
+
+interface Definition {
+  partOfSpeech: string;
+  definitions: {
+    definition: string;
+    synonyms: string[];
+    antonyms: string[];
+    example?: string;
+  }[];
+  synonyms: string[];
+  antonyms: string[];
+}
+
+
